801242cfb5c22914495faa4fe38455bcf82a3f93
Paul Graham <homosaur@gmail.com>
2012-02-18 17:25:38 -0600

    commented out GA snippet in footer by default

--- footer.php
+++ footer.php
@@ -29,13 +29,15 @@
 <!-- end scripts -->
 
 <!-- Asynchronous Google Analytics snippet. Change UA-XXXXX-X to be your site's ID.
-		 mathiasbynens.be/notes/async-analytics-snippet -->
+		 mathiasbynens.be/notes/async-analytics-snippet, uncomment to use
 <script>
 	var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
 	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
 	g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
 	s.parentNode.insertBefore(g,s)}(document,'script'));
 </script>
+
+-->
 
   <?php wp_footer(); ?>
 
