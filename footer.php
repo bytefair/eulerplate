9696513e27b86101d9f4daf1ed45fea6c3997cae
Paul Graham <homosaur@gmail.com>
2012-02-19 01:31:36 -0600

    fixed bug in footer with jquery local link

--- footer.php
+++ footer.php
@@ -21,7 +21,7 @@
 
 <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="<?php echo $GLOBALS["TEMPLATE_RELATIVE_URL"] ?>html5-boilerplate/js/jquery-1.7.1.min.js"><\/script>')</script>
+<script>window.jQuery || document.write('<script src="<?php echo $GLOBALS["TEMPLATE_RELATIVE_URL"] ?>html5-boilerplate/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
 
 <!-- scripts concatenated and minified via build script -->
 <?php versioned_javascript($GLOBALS["TEMPLATE_RELATIVE_URL"]."html5-boilerplate/js/plugins.js") ?>
